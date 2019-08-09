2fe5d82ad49b3c83c89033172f0017115ec4743f
indra <nurzamanindra@gmail.com>
2019-08-09 14:45:19 +0700

    refactor code and update test suit

--- test/pairTheShocks.test.js
+++ test/pairTheShocks.test.js
@@ -1,9 +1,9 @@
-const pair = require('../pairTheShocks');
+const pair = require('../logic/pairTheShocks');
 
 
 
 
-test('should return 3', () => {
+test('[main functionality] --> should return 3', () => {
     let HowManyData = 6;
     let Data = "12 12 12 12 13 13";
     expect(pair.pairMyShock(HowManyData,Data)).toBe(3);
@@ -20,22 +20,4 @@ test('should return an array of data', () => {
 test('should return Distinct value', () => {
     let Data = [1,1,1,2,2,2,3,3,3,];
     expect(pair.DistinctValue(Data)).toStrictEqual([1,2,3]);
-});
-
-describe('test from describe', ()=>{
-    console.log('describe this first');
-
-    expect(true).toBeTruthy();
-});
-
-
-describe('test from describe2s', ()=>{
-    console.log('describe this second');
-
-    test('should return an array of data from describe2', () => {
-        let Data = "1 2 3 12 13 14 15";
-        expect(Array.isArray(pair.splitData(Data))).toBeTruthy();
-        expect(pair.splitData(Data).length).toBe(7);
-    });
-
 });
